6fd928094a2a84b679cafb3896d5a3b21ff974e4
Hoang Thuc <hoangthuc2809@gmail.com>
2019-10-16 10:38:44 +0700

    update something

--- routes/api.php
+++ routes/api.php
@@ -17,4 +17,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('/search','RemindController@search');
+Route::post('/search','User\RemindController@search');
